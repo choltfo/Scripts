05a0b31356969fd80d2625f9b8f8438556c2a6ec
Charles Holtforster <charles.holforster@gmail.com>
2013-04-15 17:42:47 -0400

    General debugging for Objectives

--- ObjectiveSystem/Campaign.cs
+++ ObjectiveSystem/Campaign.cs
@@ -11,32 +11,8 @@ public class Campaign {
 	//public Vector3 displayCoords;
 	
 	public bool updateMissions() {
-		/*bool notDone = false;
-		foreach (Mission mission in missions) {
-			if (!mission.checkCompletion()) {
-				notDone = true;
-			}
-		}
-		if (!notDone) {
-			complete = true;
-			return true;
-		} else {
-			int lastCompleted = -1;
-			for (int i = 0; i < missions.Length; i ++) {
-				if (missions[i].complete) {
-					lastCompleted = i;
-				}
-			}
-			if (lastCompleted+1 == missions.Length) {
-				complete = true;
-				return true;
-			}
-			missions[lastCompleted+1].Begin();
-			return false;
-		}*/
-
 		//Number of the last completed mission
-		int lastCompleted = 0;
+		int lastCompleted = -1;
 		//Updates lastCompleted 
 		for (int i = 0; i < missions.Length; i++) {
 			if (missions[i].complete) {
