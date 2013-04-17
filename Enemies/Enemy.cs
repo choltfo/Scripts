8da834e119a95fc333f81318af4aab2d83179c1a
Charles Holtforster <charles.holforster@gmail.com>
2013-04-16 20:50:02 -0400

    Started working on an enemy that can shoot back.

--- Enemies/Enemy.cs
+++ Enemies/Enemy.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (CharacterController))]
+
 public class Enemy : MonoBehaviour {
 	public int usefulnessScale = 0;
 }
---
+++ Enemies/Shooter.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+
+public class Shooter : Enemy {
+	
+	public GameObject player;
+	CharacterController motor;
+	EnemyHealth health;
+	//public float effectDelay;
+	//public float recentEffectTime = 0;
+	public float TIME;
+	//public DrugEffect collideEffect;
+	//public float damage;
+	
+	/*void OnCollisionStay(Collision collision) {
+		//Debug.Log ("Hit something, specifically "+collision.gameObject.name);
+		if ((Time.time - recentEffectTime) >= effectDelay) {
+			Debug.Log ("Time is valid");
+			if (collision.gameObject.transform.FindChild("Camera").GetComponent<DrugDosage>() != null) {
+				collision.transform.FindChild("Camera").gameObject.GetComponent<DrugDosage>().addEffect(collideEffect.Duplicate());
+				collision.transform.FindChild("Camera").gameObject.GetComponent<Health>().Damage(damage);
+				Debug.Log ("Applying effect to player");
+				recentEffectTime = Time.time;
+			}
+		}
+	}*/	
+	
+	void Start() {
+		//collideEffect.UID = DrugEffect.generateUID();
+		motor = gameObject.GetComponent<CharacterController>();
+		health = GetComponent<EnemyHealth>();
+	}
+	
+	void Update () {
+		TIME = Time.time;
+		
+		float x = 0;
+		float z = 0;
+		RaycastHit hit;
+		//If something is between Enemy and Player...
+		if (Physics.Linecast(transform.position, player.transform.position, out hit) && hit.transform.gameObject!=player) {
+			if (player.transform.position.x > transform.position.x) {
+				x = 0.05f;
+			}
+			if (player.transform.position.x < transform.position.x) {
+				x = -0.05f;
+			}
+			if (player.transform.position.z > transform.position.z) {
+				z = 0.05f;
+			}
+			if (player.transform.position.z < transform.position.z) {
+				z = -0.05f;
+			}
+		}
+		Vector3 Movement = new Vector3(x,0,z);
+		
+		motor.Move(Movement);
+	}
+}
