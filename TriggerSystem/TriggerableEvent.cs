e06eace2c74d266f7dae699ab46025fb7eaa0206
Charles Holtforster <charles.holforster@gmail.com>
2013-05-04 20:19:24 -0400

    Added recursive TriggerableEvents.
    Triggerable triggers triggering triggers.

--- TriggerSystem/TriggerableEvent.cs
+++ TriggerSystem/TriggerableEvent.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 
 public class TriggerableEvent {
 	
+	public TriggerableEvent[] events;
 	public Detonator[] explosions;
 	public AudioSource soundSource;
 	public AudioClip sound;
@@ -15,6 +16,9 @@ public class TriggerableEvent {
 	public virtual void Trigger() {
 		foreach (Detonator explosion in explosions) {
 			explosion.Explode();
+		}
+		foreach (TriggerableEvent TEvent in events) {
+			TEvent.Trigger();
 		}
 		if (sound != null && soundSource != null) {
 			soundSource.PlayOneShot(sound);
