97a456012a505e330cc81c00f5f54a68a205e02c
Charles Holtforster <charles.holtforster@gmail.com>
2013-05-06 10:40:49 -0400

    Made Teleporters work better.
    Legit.

--- Transport/Teleporter.cs
+++ Transport/Teleporter.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class Teleporter : InteractObject {
 	public Teleporter linkedTeleporter;
+	public Vector3 relativePosition;
 
 	void Start () {}
 	void Update () {}
 
 	public override void Interact (GameObject player) {
-		player.transform.position = linkedTeleporter.rigidbody.position;
+		player.transform.position = linkedTeleporter.transform.position + linkedTeleporter.relativePosition;
 	}
 }
