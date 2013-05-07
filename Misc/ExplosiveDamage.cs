9f4dd8e62c74a861537481f1038192d506de2815
Charles Holtforster <charles.holforster@gmail.com>
2013-05-07 19:03:33 -0400

    Continued grenade work
    Grenade keeps exploding instantly.

---
+++ Misc/ExplosiveDamage.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class ExplosiveDamage : MonoBehaviour {
+	
+	public float range;
+	public float maxDamage;
+	
+	public ExplosiveDamage () { }
+	public void explode() {
+		GetComponent<Detonator>().Explode();
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+		print (hitColliders.Length);
+		foreach (Collider hit in hitColliders) {
+			print (hit.transform.gameObject.name);
+			float damage = Mathf.Lerp(0, maxDamage, Vector3.Distance(hit.transform.position, transform.position)
+				/range);
+			if (hit.transform.FindChild("Camera") != null) {
+				if (hit.transform.FindChild("Camera").gameObject.GetComponent<Health>() != null) {
+					hit.transform.FindChild("Camera").gameObject.GetComponent<Health>().Damage(damage);	
+				}
+			}
+			if (hit.gameObject.name == "RaycastLayer") {
+				if (hit.transform.parent.gameObject.GetComponent<EnemyHealth>() != null) {
+					hit.transform.parent.gameObject.GetComponent<EnemyHealth>().Health -= (int)damage;	
+				}
+			}
+			if (hit.transform.gameObject.GetComponent<EnemyHealth>() != null) {
+				hit.transform.gameObject.GetComponent<EnemyHealth>().Health -= (int)damage;	
+			}
+			if (hit.transform.gameObject.GetComponent<Detonator>() != null) {
+				hit.transform.gameObject.GetComponent<Detonator>().Explode();	
+			}
+			if (hit.transform.gameObject.GetComponent<ExplosiveDamage>() != null) {
+				hit.transform.gameObject.GetComponent<ExplosiveDamage>().explode();	
+			}
+		}
+		print ("BOOM");
+	}
+}
--- WeaponSystem/ThrownGrenade.cs
+++ WeaponSystem/ThrownGrenade.cs
@@ -5,17 +5,49 @@ using System.Collections;
 [RequireComponent (typeof (Detonator))]
 
 public class ThrownGrenade : MonoBehaviour {
-	
-	float delay;
-	float primeTime;
+	public float range;
+	public float maxDamage;
+	public float delay;
+	public float primeTime;
+	public bool blown = false;
 	
 	public void prime (float l_delay) {
 		primeTime	= Time.time;
 		delay		= l_delay;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if ((Time.time > (primeTime + delay)) && !blown) {
+			GetComponent<Detonator>().Explode();
+			//RaycastHit[] hits = Physics.SphereCastAll(transform.position, range, new Vector3(0,0,0));
+			Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
+			print (hitColliders.Length);
+			foreach (Collider hit in hitColliders) {
+				print (hit.transform.gameObject.name);
+				float damage = Mathf.Lerp(0, maxDamage, Vector3.Distance(hit.transform.position, transform.position)
+					/range);
+				if (hit.transform.FindChild("Camera") != null) {
+					if (hit.transform.FindChild("Camera").gameObject.GetComponent<Health>() != null) {
+						hit.transform.FindChild("Camera").gameObject.GetComponent<Health>().Damage(damage);	
+					}
+				}
+				if (hit.gameObject.name == "RaycastLayer") {
+					if (hit.transform.parent.gameObject.GetComponent<EnemyHealth>() != null) {
+						hit.transform.parent.gameObject.GetComponent<EnemyHealth>().Health -= (int)damage;	
+					}
+				}
+				if (hit.transform.gameObject.GetComponent<EnemyHealth>() != null) {
+					hit.transform.gameObject.GetComponent<EnemyHealth>().Health -= (int)damage;	
+				}
+				if (hit.transform.gameObject.GetComponent<Detonator>() != null) {
+					hit.transform.gameObject.GetComponent<Detonator>().Explode();	
+				}
+				if (hit.transform.gameObject.GetComponent<ExplosiveDamage>() != null) {
+					hit.transform.gameObject.GetComponent<ExplosiveDamage>().explode();	
+				}
+			}
+			print ("BOOM");
+			blown = true;
+		}
 	}
 }
