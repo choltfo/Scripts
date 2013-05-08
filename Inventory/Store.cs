641ac969a0cfeddadb671422e9f23f6c57a073a5
Charles Holtforster <charles.holforster@gmail.com>
2013-05-07 21:29:06 -0400

    Began on the stores.

--- Inventory/Store.cs
+++ Inventory/Store.cs
@@ -16,6 +16,11 @@ public class Store : MonoBehaviour {
 	public Inventory inventory;
 	public Inventory playerInv;
 	public Pause pauseController;
+	public StoreMode storeMode = StoreMode.Buy;
+	
+	public float weaponSlider = 0;
+	public float ammoItemSlider = 0;
+	
 	
 	public void Interact (ShootObjects player) {
 		playerInv = player.inventory;
@@ -27,6 +32,45 @@ public class Store : MonoBehaviour {
 		if (Time.timeScale == 0) {
 			if (pauseController.pane == "/Store/"+UID.ToString()) {
 				
+				if (GUI.Button(new Rect(0,25,100,100),"Resume")) {
+					pauseController.pane = "/Pause";
+					Time.timeScale = 1f;
+					storeMode = StoreMode.Buy;
+				}
+				
+				if (GUI.Button(new Rect(Screen.width/2-50,100,50,25), "Buy")) {
+					storeMode = StoreMode.Buy;
+				}
+				if (GUI.Button(new Rect(Screen.width/2,100,50,25), "Sell")) {
+					storeMode = StoreMode.Sell;
+				}
+				
+				//Weapons
+				weaponSlider = GUI.VerticalScrollbar(new Rect(Screen.width/2-15, 125, 25, 125),
+					weaponSlider, 1.0F, 0.0F, 10.0F);
+				
+				GUI.Box(new Rect(Screen.width/2-165, 125, 165, 125), "");
+				
+				//Items/ammo
+				ammoItemSlider = GUI.VerticalScrollbar(new Rect(Screen.width/2+165, 125, 25, 125),
+					ammoItemSlider, 1.0F, 0.0F, 10.0F);
+				
+				GUI.Box(new Rect(Screen.width/2, 125, 165, 125), "");
+				
+				if (storeMode == StoreMode.Buy) {
+					int i = 0;
+					foreach (Weapon weapon in inventory.weapons) {
+						GUI.Button(new Rect(Screen.width/2-165, 125+(25*i), 150, 25), weapon.DisplayName);
+						i++;
+					}
+				}
+				if (storeMode == StoreMode.Sell) {
+					int i = 0;
+					foreach (Weapon weapon in playerInv.weapons) {
+						GUI.Button(new Rect(Screen.width/2-165, 125+(25*i), 150, 25), weapon.DisplayName);
+						i++;
+					}
+				}
 			}
 		}
 	}
@@ -36,4 +80,9 @@ public class Store : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {}
+}
+
+public enum StoreMode {
+	Buy,
+	Sell
 }
