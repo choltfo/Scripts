9848e13b6fcdb8676666938c09d6b6d2199a2ac3
Ertjaet <ertjaet@gmail.com>
2013-05-08 20:12:34 -0400

    Removed Trampolines. :(

--- Transport/Teleporter.cs
+++ Transport/Teleporter.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof (Rigidbody))]
-
 public class Teleporter : InteractObject {
 	public Teleporter linkedTeleporter;  // The teleporter this is linked to
 	public Vector3 relativePosition;  // How far away the player is moved when teleported to this teleporter
-	public bool isTrampoline = true;  // Whether this is a trampoline
 	public CameraFade fader;
 	public Color fadeColor;
 	public Color transparency;
@@ -20,14 +17,6 @@ public class Teleporter : InteractObject {
 		fader.SetScreenOverlayColor(fadeColor);
 		beginFadeAt = Time.time;
 		faded = false;
-		if (isTrampoline) {
-			print("This is a trampoline");
-			relativePosition.y += 100;
-		}
-		else {
-			print("This is not a trampoline");
-		}
-
 	}
 	public void Update () {
 		if (beginFadeAt + fadeTime > Time.time && !faded) {
