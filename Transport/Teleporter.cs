a8a233baac1fff22978ab679059facd5a49bab3f
Ertjaet <ertjaet@gmail.com>
2013-05-29 16:30:44 -0400

    Started on Project: Haiku

--- Transport/Teleporter.cs
+++ Transport/Teleporter.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// A teleporter
+/// Transports the player to the
+/// Other location
+/// </summary>
 public class Teleporter : InteractObject {
 	public Teleporter linkedTeleporter;  // The teleporter this is linked to
 	public Vector3 relativePosition;  // How far away the player is moved when teleported to this teleporter
@@ -8,11 +13,22 @@ public class Teleporter : InteractObject {
 	public Color fadeColor;
 	public Color transparency;
 	public float fadeTime;
+
+	/// <summary>
+	/// A boolean var
+	/// Whether activated on
+	/// Collision event
+	/// </summary>
 	public bool isPortal = false;
 	
 	bool faded = true;
 	float beginFadeAt;
 
+	/// <summary>
+	/// When interacted
+	/// By the player clicking 'E'
+	/// This is what happens
+	/// </summary>
 	public override void Interact (GameObject player) {
 		if (isPortal) {
 			return;
@@ -22,6 +38,12 @@ public class Teleporter : InteractObject {
 		beginFadeAt = Time.time;
 		faded = false;
 	}
+
+	/// <summary>
+	///	Teleports player
+	/// On a collision event
+	/// If isPortal true
+	/// </summary>
 	public void OnTriggerEnter(Collider thing){
 		if (isPortal) {
 			thing.transform.root.position = linkedTeleporter.transform.position + linkedTeleporter.relativePosition;
