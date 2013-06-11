aa5234bb729d3e9c8947d30340daf001e1418db2
Charles Holtforster <charles.holforster@gmail.com>
2013-06-11 17:33:46 -0400

    worked on improving explosions and grenades

--- WeaponSystem/ThrownGrenade.cs
+++ WeaponSystem/ThrownGrenade.cs
@@ -49,48 +49,10 @@ public class ThrownGrenade : MonoBehaviour {
 	/// Detonate this grenade. Right now.
 	/// </summary>
 	void detonate () {
-		//RaycastHit[] hits = Physics.SphereCastAll(transform.position, range, new Vector3(0,0,0));
-		Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
-		//print (hitColliders.Length);
-		foreach (Collider hit in hitColliders) {
-			//print (hit.transform.gameObject.name);
-			float damage = Mathf.Lerp(0, maxDamage, Vector3.Distance(hit.transform.position, transform.position)/range);
-			if (hit.transform.FindChild("Camera") != null) {
-				if (hit.transform.FindChild("Camera").gameObject.GetComponent<Health>() != null) {
-					hit.transform.FindChild("Camera").gameObject.GetComponent<Health>().Damage(damage, DamageCause.Explosion);	
-				}
-			}
-			if (hit.gameObject.name == "RaycastLayer") {
-				if (hit.transform.parent.gameObject.GetComponent<EnemyHealth>() != null) {
-					hit.transform.gameObject.GetComponent<EnemyHealth>().damage(damage, DamageCause.Explosion);
-				}
-			}
-			if (hit.transform.gameObject.GetComponent<EnemyHealth>() != null) {
-				hit.transform.gameObject.GetComponent<EnemyHealth>().damage(damage, DamageCause.Explosion);
-			}
-			if (hit.transform.gameObject.GetComponent<Detonator>() != null) {
-				hit.transform.gameObject.GetComponent<Detonator>().Explode();	
-			}
-			if (hit.transform.gameObject.GetComponent<ExplosiveDamage>() != null) {
-				hit.transform.gameObject.GetComponent<ExplosiveDamage>().explode();	
-			}
-			if (hit.transform.gameObject.GetComponent<ExplosiveDamage>() != null) {
-				if (!hit.transform.gameObject.GetComponent<ExplosiveDamage>().blown) {
-					hit.transform.gameObject.GetComponent<ExplosiveDamage>().explode();
-				}
-			}
-		}
-		//print ("BOOM");
 		blown = true;
-		GetComponent<Detonator>().Explode();
+		GetComponent<ExplosiveDamage>().explode();
 	}
 	
-	/// <summary>
-	/// Raises the collision enter event.
-	/// </summary>
-	/// <param name='collision'>
-	/// Collision.
-	/// </param>
 	void OnCollisionEnter(Collision collision){
 		if (detonateOnCollision) {
 			detonate();
@@ -100,7 +62,6 @@ public class ThrownGrenade : MonoBehaviour {
 	void Update () {
 		if ((Time.time > (primeTime + delay)) && !blown && !detonateOnCollision) {
 			detonate();
-			Destroy (this);
 		}
 	}
 }
