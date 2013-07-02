96db2537c77c2190c36423c45632b9993f970093
Charles Holtforster <charles.holforster@gmail.com>
2013-07-01 22:27:46 -0400

    Began adding risk assessments

--- PFSystem/PFNode.cs
+++ PFSystem/PFNode.cs
@@ -9,9 +9,14 @@ public class PFNode : MonoBehaviour {
 public class PFNodeEntry {
 	public PFNode node;
 	public bool accessible;
+	public float riskfactor = 0;
 	
-	public float riskAssessment (GameObject[] enemies) {
-		
+	public float riskAssessment (GameObject[] enemies, PFNode otherNode) {
+		foreach (GameObject enemy in enemies) {
+			riskFactor += Vector3.Distance(enemy.transform.postition, node.transform.position);
+			riskFactor += Vector3.Distance(enemy.transform.postition, otherNode.transform.position);
+		}
+		riskFactor += Vector3.Distance(node.transform.postition, otherNode.transform.position);
 	}
 	
 	public bool accessibilityAssessment (PFNode otherNode) {
