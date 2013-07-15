88e460da1b4625951a4fcdf405681f0d4d492179
Charles Holtforster <charles.holforster@gmail.com>
2013-07-14 21:05:24 -0400

    Radio's are now valid InteractObjects.
    They should be InteractObjectives, though, IMO.

--- Gameplay/Radio.cs
+++ Gameplay/Radio.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 
 public class Radio : InteractObject {
 
-	public AudioClip[] clips;
+	public List<AudioClip> clips;
 	public bool radioOn;
 	
 	static bool debug = true;
 	
+	public TriggerableEvent[] events;
+	public SubtitleController SubTitleController;
+	bool triggered = false;
+	
 	// Use this for initialization
 	void Start () {
 		if (radioOn) {
-			audio.clip = clips[Random.Range(0,clips.Length)];
+			audio.clip = clips[Random.Range(0,clips.ToArray().Length)];
 			audio.Play();
 			if (debug) print("Starting Radio");
 		}
@@ -23,7 +28,7 @@ public class Radio : InteractObject {
 	void Update () {
 		if (radioOn && !audio.isPlaying) {
 			if (debug) print("Playing new clip, as audio.isPlaying = " + audio.isPlaying);
-			audio.clip = clips[Random.Range(0,clips.Length)];
+			audio.clip = clips[Random.Range(0,clips.ToArray().Length)];
 			audio.Play();
 		}
 		if (!radioOn && audio.isPlaying) {
@@ -34,6 +39,10 @@ public class Radio : InteractObject {
 	
 	public override void Interact (GameObject player) {
 		radioOn = !radioOn;
+		
+		if (!triggered) {
+			foreach (TriggerableEvent e in events) e.Trigger(SubTitleController);
+		}
 		if (debug) print("Toggling radio");
 	}
 }
