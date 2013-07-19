dc261f7943fc5aac8c311d79583dac8a7285fac5
Charles Holtforster <charles.holforster@gmail.com>
2013-07-18 20:23:58 -0400

    Fixed it!
    WOOT! First AI player kill!

--- Enemies/ShootingEnemy.cs
+++ Enemies/ShootingEnemy.cs
@@ -68,19 +68,25 @@ public class ShootingEnemy : PathfindingEnemy {
 	}
 	
 	
-	// So, warning, this will make your enemy shoot himself after killing all other enemies. 
+	
 	Enemy getNearestEnemy() {
 		int nearest = 0;
 		float closest = 1000;
 		
 		int i = 0;
+		
+		// So, warning, this will make your enemy shoot himself after killing all other enemies. 
+		
 		foreach (Enemy e in targets) {
-			if (Vector3.Distance(gameObject.transform.position, targets[i].transform.position) < closest && e.faction != faction) {
+			if (Vector3.Distance(gameObject.transform.position, targets[i].transform.position) < closest &&
+					!e.faction.Equals(faction) && e != this) {
+				
+				print ("Contemplating " + e.name);
 				nearest = i;
 				closest = Vector3.Distance(gameObject.transform.position, targets[i].transform.position);
-				
-				i ++;
 			}
+			
+			i ++;
 		}
 		if (debug) print("Nearest enemy is " + targets[nearest].name);
 		return targets[nearest];
