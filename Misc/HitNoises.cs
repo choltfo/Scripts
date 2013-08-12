9c876c8c9904866a16c0665ef5b121da645d660f
Charles Holtforster <charles.holforster@gmail.com>
2013-08-12 09:25:58 -0400

    Hitnoises fixed

--- Misc/HitNoises.cs
+++ Misc/HitNoises.cs
@@ -5,11 +5,10 @@ using System.Collections;
 /// Makes stuff make nois when it hits other stuff.
 /// </summary>
 public class HitNoises : MonoBehaviour {
-	public AudioSource source;
 	public AudioClip clip;
 	
 	void OnCollisionEnter (Collision collision) {
-		source.PlayOneShot(clip);
+		audio.PlayOneShot(clip);
 	}
 
 }
