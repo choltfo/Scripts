17865ee52bec5791a9af43b1010ee928a33510a1
Charles Holtforster <charles.holforster@gmail.com>
2013-09-03 16:44:52 -0400

    Changed door system.

--- Misc/Door.cs
+++ Misc/Door.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class Door : InteractObject {
+public class Door : MonoBehaviour {
 	
 	public bool isOpen;
 	public Vector3 ClosedRotation;
@@ -10,24 +10,28 @@ public class Door : InteractObject {
 	public Vector3 OpenPosition;
 	
 	public bool autoClose = false;
-	
-	public override void Interact (GameObject player, ShootObjects SOPlayer) {
-		isOpen = !isOpen;
-	}
-	
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public float autoCloseTime = 10f; 
+	float lastOpen = 0f;
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		
 		if (isOpen) {
 			transform.localPosition = Vector3.Lerp(transform.localPosition, OpenPosition, Time.deltaTime);
 			transform.localEulerAngles = Vector3.Slerp(transform.localRotation.eulerAngles, OpenRotation, Time.deltaTime);
+			if (Time.time > lastOpen + autoCloseTime) {
+				isOpen = false;
+			}
 		} else {
 			transform.localPosition = Vector3.Lerp(transform.localPosition, ClosedPosition, Time.deltaTime);
 			transform.localEulerAngles = Vector3.Slerp(transform.localRotation.eulerAngles, ClosedRotation, Time.deltaTime);
 		}
+		
+		
+	}
+	
+	public void setState (bool state) {
+		isOpen = state;
+		lastOpen = Time.time;
 	}
 }
