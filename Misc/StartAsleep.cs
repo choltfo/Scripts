651304a4f8e76f3d3922e5c440cdbdb846e3d2fe
Charles Holtforster <charles.holforster@gmail.com>
2013-09-03 16:49:42 -0400

    Added a few spare things.

---
+++ Misc/StartAsleep.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartAsleep : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.Sleep();
+	}
+}
---
+++ Misc/StartUnPaused.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class StartUnPaused : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
