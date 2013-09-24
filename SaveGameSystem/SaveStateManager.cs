05fa4f42c09cdd24429c079ead2893d4fbbb0c7e
Charles Holtforster <charles.holforster@gmail.com>
2013-09-24 16:43:56 -0400

    Trying again
    From the ground up this time. May be woefully underpowered.

--- SaveGameSystem/SaveStateManager.cs
+++ SaveGameSystem/SaveStateManager.cs
@@ -4,92 +4,85 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using System.IO;
+using System;
 
 public class SaveStateManager : MonoBehaviour {
+	public string savePath = @"Index.txt";
 	
-	public string[] savePaths;
-	
-	public string savePathsPath = @"Index.txt";
-	
-	public void findFiles () {
-		savePaths = System.IO.File.ReadAllLines(savePathsPath);
+	public void load () {
+		String line;
+		try {
+			//Pass the file path and file name to the StreamReader constructor
+			StreamReader sr = new StreamReader(savePath);
+			//Read the first line of text
+			line = sr.ReadLine();
+
+			//Continue to read until you reach end of file
+			while (line != null) {
+				//write the lie to console window
+				Debug.log("LOAD: " + line);
+				loadPerGO (line);
+				//Read the next line
+				line = sr.ReadLine();
+			}
+				//close the file
+			sr.Close();
+		}
+		catch(Exception e) {
+			Console.WriteLine("Exception: " + e.Message);
+		}
+   		finally {
+			Console.WriteLine("Executing finally block.");
+		}
 	}
 	
-	public object load (int index, object obj) {
-		obj = DeSerialize(System.IO.File.ReadAllText(savePathsPath) +
-			GetGameObjectPath(((Component)obj).gameObject) + obj.GetType().ToString(), obj.GetType());
+	public void loadPerGO (string S) {
 		
-		return obj;
-	}
-	
-	public void save (string path) {
-		Object[] objects = Object.FindObjectsOfType(typeof(GameObject));
-		XmlDocument doc = new XmlDocument();
-		foreach (Object obj in objects) {
-			if (obj is Component) {
-				GameObject go = (GameObject)obj;
-				if (!go.isStatic) {
-					Component[] comps =  go.GetComponents<Component>();
+		string[] parts = S.Split("$");		// Seperates path and each data block.
+											// Each type of compoenent must have a specific code section programmed below.
+		string path = parts[0];
+		string[] objs = new string[parts.Length-1];
+		Array.copy(parts, 1, objs, 0, parts.Length-2);	// Copy each part of the string that isn't the path to an array.
+		
+		GameObject go = GameObject.Find(path);
+		
+		if (go != null) {
+			foreach (String s in objs) {	// For each component declaration in the string...
+				string[] vals = val.Split(":");	// Get each relevant set of values...
+				string type = vals[0];			// And the type.
+				
+				Component c = go.GetComponent(type);
+				
+				switch (typeof(c)) {
+					case typeof(Transform) :
+						string position = vals[1];
+						string rotation = vals[2];
+						string scale    = vals[3];
+						
+						string[] posStrings = position.Split(",");
+						
+						float xpos = float.Parse(posStrings[0]);
+						float ypos = float.Parse(posStrings[1]);
+						float zpos = float.Parse(posStrings[2]);
 					
-					foreach (Component comp in comps) {
-						System.IO.StreamWriter file = new System.IO.StreamWriter("Assets/Resources/SaveGames/"+System.IO.File.ReadAllText("Assets/Resources/SaveGames/"+savePathsPath) +
-			GetGameObjectPath(((Component)obj).gameObject) + obj.GetType().ToString());
-						file.WriteLine(Serialize(comp) );
-					}
+						gameObject.transform.localPosition = new Vector3(xpos, ypos, zpos);
+						// IMPORTANT: THIS MUST BE LocalEulerAngles!
+						string[] rotStrings = rotation.Split(",");
+						
+						float xrot = float.Parse(rotStrings[0]);
+						float yrot = float.Parse(rotStrings[1]);
+						float zrot = float.Parse(rotStrings[2]);
 					
+						gameObject.transform.Rotate(xrot, yrot, zrot);
+						
+						
+					break;
 				}
+				
 			}
 		}
 		
-		System.IO.StreamWriter fileL = new System.IO.StreamWriter("Assets/Resources/SaveGames/"+System.IO.File.ReadAllText("Assets/Resources/SaveGames/"+savePathsPath) +
-				GetGameObjectPath(gameObject) + @"\" +gameObject.transform.GetType().ToString());
-		fileL.WriteLine(Serialize(gameObject.transform));
-		
+		return;	// Why not?
+	
 	}
-	
-	// duck on unity answers
-	
-	public static string GetGameObjectPath(GameObject obj) {
-		string path = "/" + obj.name;
-		while (obj.transform.parent != null) {
-			obj = obj.transform.parent.gameObject;
-			path = "/" + obj.name + path;
-		}
-		return path;
-	}
-	
-	
-	/*
-	 * 		The plan:
-	 * 		Have an iteration through all objects, storing them in a flatfile with a
-	 * 		path and component type, followed by a colon and the serialized value.
-	 * 
-	 */
-	
-	
-	
-	// This bit by RShackleton on the Unity3D forums! 
-	public static string Serialize (object obj)
-{
-    StringBuilder xml = new StringBuilder();
-    XmlSerializer serializer = new XmlSerializer(obj.GetType());
-    using (TextWriter textWriter = new StringWriter(xml))
-    {
-        serializer.Serialize(textWriter, obj);
-    }
-    return xml.ToString();
-}
- 
-
-public static object DeSerialize (string xml, System.Type type)
-{
-    object obj;
-    XmlSerializer deserializer = new XmlSerializer(type);
-    using (TextReader textReader = new StringReader(xml))
-    {
-        obj = deserializer.Deserialize(textReader);
-    }
-		return obj;
-}
-		
 }
