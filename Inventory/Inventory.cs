c7e3ca3f57f9a8b483fc6defc626e9d44019a104
Charles Holtforster <charles.holforster@gmail.com>
2013-09-24 21:35:59 -0400

    Started trying to serialize weapons....
    This isn't working, we need a UID system.

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -16,12 +16,22 @@ public class Inventory {
 	
 	// Receives all the data sections after 'Shootobjects:'
 	public void load (string[] data) {
+		int dataPoint = 0;
+		for (int i = 0; i < ammo.Length; i++) {	// Pull all the ammo data, and continue.
+			ammo[i] = int.Parse(data[i]);
+			dataPoint ++;
+		}
 		
+		loadWeapons(data[dataPoint+1], data[dataPoint+2]);
+		
+		dataPoint+=2;
+		
+		// The next one
 		return;
 	}
 	
 	public string save () {
-		string output = "ShootObjects:";
+		string output = "ShootObjects:AMMO:";
 		
 		int i = 0;
 		foreach (int am in ammo) {
@@ -31,18 +41,46 @@ public class Inventory {
 		}
 		output += ":";
 		
+		output += saveWeapons() + ":";
 		
 		
 		return output;
+	}
+	
+	public string saveWeapons () {
+		string weaponZero = weapons[0].SerializeXml();
+		string[] weaponZeroAsArray = weaponZero.Split('\n');
+		weaponZero = String.Join("",weaponZeroAsArray);
+		weaponZeroAsArray = weaponZero.Split('\r');
+		weaponZero = String.Join("",weaponZeroAsArray);
+		
+		string weaponOne = weapons[0].SerializeXml();
+		string[] weaponOneAsArray = weaponZero.Split('\n');
+		weaponOne = String.Join("",weaponZeroAsArray);
+		weaponOneAsArray = weaponZero.Split('\r');
+		weaponOne = String.Join("",weaponZeroAsArray);
+		
+		return "WEAPONS#"+weaponZero+"#"+weaponOne;
+	}
+	
+	public void loadWeapons (string W1, string W0) {
+		loadWeapon(W0, 0);
+		loadWeapon(W1, 1);
+	}
+	
+	public void loadWeapon (string xml, int index) {
+		weapons[index] = XmlSupport.DeserializeXml<Weapon>(xml);
 	}
 	
 	public string saveGrenades () {
-		string output;
+		string output = "";
+		
+		
 		
 		return output;
 	}
 	
-	public void loadFrenades (string input) {
+	public void loadGrenades (string input) {
 		
 	}
 }
