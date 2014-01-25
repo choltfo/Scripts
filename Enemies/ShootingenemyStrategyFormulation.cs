3eecb2d9b0b70eac039869db06d94abe71ae84fa
Charles Holtforster <charles.holtforster@gmail.com>
2014-01-24 21:53:32 -0500

    Started on creation of enemy automization
    So, here is the plan:
    IN the preliniary stages, enemies are going to be soft-coded with a
    StateActionTrain, which will describe the action to be taken in case of
    ______. This will require the addition of a recursive search in PFNC,
    and possibly the addition of something to allow for enemies to select
    between potential targets.
    Also, another thing that needs adding is a feature by which enemies can
    neglect a target and start firing upon an enemy who becomes more
    obvious.

---
+++ Enemies/ShootingenemyStrategyFormulation.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingenemyStrategyFormulation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class ThoughtProcess {
+
+
+
+}
+
+public class StateActionTrain {
+	public State S = State.Heard;
+	public Action[] A = {Action.Burst, Action.Cover};
+}
+
+public enum State {
+	Shot,
+	Heard,
+	Viewed
+}
+
+public enum Action {
+	OpenFire,
+	Burst,
+	Cover
+}
+
+public enum PreCoverShot {
+	ButstToCover
+}
+
+public enum NodeSelection {
+	Closest,
+	ClosestCover,
+	ClosestStanding,
+	ClosestCrouching,
+	CrouchingNearestEnemy,
+	StandingNeatestEnemy,
+	CrouchingFurthestEnemy,
+	StandingFurthestEnemy
+
+}
