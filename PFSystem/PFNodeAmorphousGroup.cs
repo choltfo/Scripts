59b86b133e982f25efb16f372081d13f17227aa2
Charles Holtforster <charles.holforster@gmail.com>
2014-04-21 10:25:25 -0400

    Amorphus generator is not working yet.

--- PFSystem/PFNodeAmorphousGroup.cs
+++ PFSystem/PFNodeAmorphousGroup.cs
@@ -5,20 +5,19 @@ using System.Collections;
 public class PFNodeAmorphousGroup : MonoBehaviour {
 	public void SetupGroup() {
 		PFNode[] PFNodes = GetComponentsInChildren<PFNode>();
+		print (PFNodes.Length);
 		for (int i = 0; i < PFNodes.Length; i++) {
 			PFNodes[i].Nodes = new PFNodeEntry[PFNodes.Length-1];
+			print("Working at "+PFNodes[i].name+".");
 			for (int o = 0; o < PFNodes.Length; o++) {
 				print("Attempting to connect "+i+" and "+o+".");
+				PFNode n0 = PFNodes[o];
 				if (o > i) {
-					print ("o > i, subtracted add.");
-					PFNodes[i].Nodes[o-1].node = PFNodes[o];
+					PFNodes[i].Nodes[o-1].Equals(null);
 					PFNodes[i].Nodes[o-1].distance = Vector3.Distance(PFNodes[i].transform.position, PFNodes[o].transform.position);
 				} else if (o < i) {
-					print ("o < i, normal add.");
 					PFNodes[i].Nodes[o].node = PFNodes[o];
 					PFNodes[i].Nodes[o].distance = Vector3.Distance(PFNodes[i].transform.position, PFNodes[o].transform.position);
-				} else {
-					print ("Failed!");
 				}
 			}
 		}
